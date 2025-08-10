7a541577aa8367aa29b769e7105ae18ee8c636e2
Muhammad Ali <aliyounas343250@gmail.com>
2025-08-10 08:19:19 +0300

    new updates

--- src/models/user..model.js
+++ src/models/user..model.js
@@ -54,4 +54,44 @@ const userSchema = new Schema({
 },
 )
 
+userSchema.pre("save" ,async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.method.isPasswordCorrect = async function (password){
+   return await bcrypt.compare(password,this.password)
+}
+
+userSchema.method.generateAccessToken = function (){
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.username
+
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methodsgenerateRefreshToken = function (){
+    return jwt.sign({
+        _id: this._id,
+        
+    },
+    process.env.REFRESH_TOKEN_EXPIRY,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
+
+
+
+
+
 export const User = mongoose.model("User", userSchema)
